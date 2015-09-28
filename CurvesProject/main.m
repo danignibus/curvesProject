ba09b61c09859b46cca45b4d8d570a4630db4ede
Dani Gnibus <Danielle.N.Gnibus.17@dartmouth.edu>
2015-09-28 20:14:38 +0200

    Initial Commit

---
+++ CurvesProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CurvesProject
+//
+//  Created by Dani Gnibus on 9/28/15.
+//  Copyright (c) 2015 Dani Gnibus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
